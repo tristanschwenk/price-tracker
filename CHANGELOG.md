f8a3fc3c60e7f8129d2741ca0864fb71388dd3e5
Zao Soula <contact@zaosoula.fr>
2021-04-26 09:54:31 +0200

    0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## v0.0.2 - 2020-04-26
+### Added
+- CHANGELOG.md file
+- .nvmrc - Define node version to 15.2.1
+- .eslintrc.js - Airbnb style configuration
+- .env - Define env vars
+- Insert product with price or if the product already exists, push new price
+
+### Changed
+- Product model: `prices` `value` from `number` to `string`
+
+## v0.0.1 - 2020-04-23
+### Added
+- Scapper:
+  - Product model
+  - Input url format check
+  - Fetch ASIN, name, price
+  - Insert product in BDD 
--- index2.js
+++ index2.js
@@ -1,87 +1,122 @@
-const puppeteer = require('puppeteer')
-const readline = require('readline');
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const Model = mongoose.model
+/* eslint-disable no-continue */
+/* eslint-disable no-constant-condition */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-console */
+import dotenv from 'dotenv';
 
-mongoose.connect('mongodb+srv://priceTracker:qatJoh-zukrys-medby8@cluster0.xk28e.mongodb.net/priceTracker', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
+import mongoose from 'mongoose';
+
+import puppeteer from 'puppeteer';
+import readline from 'readline';
+
+dotenv.config();
+
+const { Schema, model: Model } = mongoose;
+
+console.log = ((oldConsoleLog) => (
+  (data, ...args) => {
+    if (process.env.DEBUG) oldConsoleLog(data, ...args);
+  }
+))(console.log);
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
 const ProductSchema = new Schema({
-    url: String,
-    name: String,
-    asin: String,
-    prices: [{
-        timestamp: Number,
-        value: Number,
-    }],
-})
+  url: String,
+  name: String,
+  asin: String,
+  prices: [{
+    timestamp: Number,
+    value: String,
+  }],
+});
 
 const Product = new Model('Product', ProductSchema);
 
-const userInput = (question) => {
-  return new Promise((resolve, reject) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    rl.question(question, (answer) => {
-      rl.close();
-      resolve(answer);
-    });
+const userInput = (question) => new Promise((resolve) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
-};
 
-
-
-
+  rl.question(question, (answer) => {
+    rl.close();
+    resolve(answer);
+  });
+});
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  process.on('SIGINT', async function () {
+  process.on('SIGINT', async () => {
     await browser.close();
     process.exit();
   });
 
-
   while (true) {
-    const url = await userInput('URL: ');
+    console.log('/======');
+    const url = await userInput('| URL: ');
+
     let pageUrl;
 
     try {
       pageUrl = new URL(url);
 
-      if(!pageUrl.host.match(/(.*\.)?amazon\.\w/)) {
+      if (!pageUrl.host.match(/(.*\.)?amazon\.\w/)) {
         console.log('Incorrect url (expecting an amazon one)');
+        console.log('\\======\n');
         continue;
       }
     } catch (e) {
       console.log('Incorrect url (expecting an amazon one)');
+      console.log('\\======\n');
       continue;
     }
 
+    console.log('\\======\n');
     await page.goto(url);
 
-    const asin = (await page.$eval('[name=ASIN]', el => el.value))
-          name = (await page.$eval('#productTitle', el => el.textContent)).replace(/\n/g, '')
-          price = (await page.$eval('#priceblock_ourprice', el => el.textContent));
+    const asin = (await page.$eval('[name=ASIN]', (el) => el.value));
+    const name = (await page.$eval('#productTitle', (el) => el.textContent)).replace(/\n/g, '');
+    const price = (await page.$eval('#priceblock_ourprice', (el) => el.textContent));
 
-    console.log(price);
-    const knownProduct = await Product.exists({asin});
+    console.log('/======');
+    console.log(`| ASIN: ${asin}`);
+    console.log(`| NAME: ${name}`);
+    console.log(`| PRICE: ${price}`);
+    console.log('|======');
 
-    if(!knownProduct) {
-      const product = await (new Product({
+    let product = await Product.findOne({ asin });
+
+    if (!product) {
+      product = new Product({
         asin,
         name,
-        url
-      })).save();
-      console.log(`Product "${name}" added.`);
+        url,
+        prices: [{
+          timestamp: Date.now(),
+          value: price,
+        }],
+      });
+
+      await product.save();
+
+      console.log('| Product added.');
+      console.log('\\======\n');
+    } else {
+      product.prices.push({
+        timestamp: Date.now(),
+        value: price,
+      });
+
+      await product.save();
+
+      console.log('| Price pushed to product.');
+      console.log('\\======\n');
     }
   }
-
 })();
