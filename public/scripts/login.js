71eb56657db539bba5344f7c4a53ab401c28cce2
Tristan264 <tristan.bschwenk@icloud.com>
2021-05-10 19:20:30 +0200

    added user token to log as a user

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,23 +1,28 @@
+import ApiService from './api.service.js';
+
+const apiService = new ApiService();
+
+
 var app = new Vue({
     el: '#app',
     data: {
       email: "",
       password: ""
     },
-    async beforeMount() {
-      await this.getProduct();
-    },
     methods: {
       login: async function () {
-        const login = await this.callApi({
+        const login = await apiService.execute({
           "method": "auth.login",
           "body": {
               email: this.email,
               password: this.password
           }
-        });
+        }, false);
         
-        console.log(login);
+        if(login['auth.login']){
+            localStorage.setItem('accessToken', login['auth.login'].token)
+            window.location.pathname="/"
+        }
       },
     }
   })
